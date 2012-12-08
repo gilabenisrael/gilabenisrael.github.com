8e6b9afc9dfbff57e83ed5d43c2448a412c51e6c
David Ben-Israel <benisrael.david@gmail.com>
2012-12-08 23:18:58 +0200

    change JPG to jpg2

--- assets/img/drawings/old drawing list/make-thumbnails.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-ls | xargs -n1 | sed -e 's/\.jpg//' | xargs -n1 -I% convert -thumbnail 200 %.jpg %.thumb.jpg
-
--- assets/img/drawings/old drawing list/resize-images.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-ls | xargs -n1 | sed -e 's/\.jpg//' | xargs -n1 -I% convert -resize x400 %.jpg %.4.jpg
-
